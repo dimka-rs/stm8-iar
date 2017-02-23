5270092c0627c489b83c518b8519496c96f88b84
Dmitry K <dimka.rs@gmail.com>
2017-02-24 02:21:11 +0300

    test als311 project fixes

--- test-project/main.c
+++ test-project/main.c
@@ -1,21 +1,29 @@
 #include "iostm8s103f3.h"
 
-//PORT C
-#define seg_H 7
-#define seg_G 6
-#define seg_F 5
-#define seg_E 4
-#define seg_D 3
+//PORT C Anodes, active high
+#define seg_H 7  //HS
+#define seg_G 6  //HS
+#define seg_F 5  //HS
+#define seg_E 4  //HS
+#define seg_D 3  //HS
+
 //PORT A
-#define seg_C 3
+#define seg_C 3  //HS
 #define seg_B 2
 #define seg_A 1
-//PORT D left to right!
-#define dig_0 2
-#define dig_1 3
-#define dig_2 4
-#define dig_3 5
-#define dig_4 6
+
+//PORT D left to right! Cathodes, active low
+//#define SWIM 1  //HS SWIM! do not use
+#define dig_0 2  //HS
+#define dig_1 3  //HS
+#define dig_2 4  //HS
+#define dig_3 5  //HS
+#define dig_4 6  //HS
+
+//PORT B - usonic
+#define usin  4  // T, also SCL
+#define usout 5  // T, also SDA - will not work, use seg_H for now
+
 
 //Digits
 extern const char pd[5] = {
@@ -66,19 +74,49 @@ extern const char pc[16] = {
 /*f*/ (1 << seg_E) | (1 << seg_F) | (1 << seg_G),
 };
 
-void SomeDelay() {
-  for (unsigned long delay_count=0; delay_count<30000; delay_count++);
+void Delay(unsigned long max_delay) {
+  for (unsigned long delay_count=0; delay_count<max_delay; delay_count++);
   };
 
 void WriteChar(unsigned char pos, unsigned char val) {
+  //turn segs off for a while
+  PD_ODR = PD_ODR  | 0x7C ;
+  Delay(20);
   PD_ODR = pd[pos];
   PC_ODR = pc[val];
   PA_ODR = pa[val]; 
   };
 
+unsigned int usmeasure()
+{
+  unsigned int cnt, cm = 0;
+  unsigned char pin = 0;
+  //send pulse
+  unsigned char pcodr = PC_ODR;
+  PC_ODR = (1 << seg_H);
+  Delay(1);
+  PC_ODR = pcodr;
+  //recv echo. max 4 m = 23200 us
+  //wait for HIGH
+  for (unsigned int i=0; i<60000; i++) {
+    pin = PB_IDR & (1 << usin);
+    if (pin != 0) { break ;}
+  };
+  //count HIGH length
+  for (unsigned int i=0; i<3000; i++) {
+    pin = PB_IDR & (1 << usin);
+    cnt = cnt + 1;
+    if (pin == 0) { break ;}
+  };
+  // cm = us/58, eq / 29 / 2
+  // my guess is * 8.1 -60 additionnaly
+  // which results in / 7.16 - 60
+  cm = cnt / 7.16 - 60 ;
+  return cm;
+};
+
 int main( void )
 {
-  //PB_DDR_bit.DDR5 = 1;
   PC_DDR = 0xF8; // Set to out
   PC_CR1 = 0xF8; // Push-Pull
   PC_CR2 = 0x00; // Slow mode
@@ -88,15 +126,34 @@ int main( void )
   PD_DDR = 0x7C; // Set to out bits 2-6
   PD_CR1 = 0x00; // Open drain
   PD_CR2 = 0x00;
+  unsigned int a = 0;
   
   while(1) {
-    for (unsigned int i=0; i<16; i++) {
-      WriteChar((i%5), i);
-      SomeDelay();
+    unsigned int cm = usmeasure();
+    unsigned int d100 = cm / 100;
+    unsigned int d10 = (cm % 100) / 10;
+    unsigned int d1 = cm % 10;
+    //Display
+    for (unsigned int i=0; i<300; i++) {
+      WriteChar(0, d100);
+      Delay(10);
+      WriteChar(1, d10);
+      Delay(10);
+      WriteChar(2, d1);
+      Delay(10);
+      WriteChar(4, a);
+      Delay(10);
+      PD_ODR = PD_ODR  | 0x7C ; //turn off while measure
     };
+    //increment indicator counter
+    if (a < 9) { a = a + 1 ;} else { a = 0 ;}
 
-    //PC_ODR ^= MASK_PC_ODR_ODR5;
+    // Running digits test
+    //for (unsigned int i=0; i<16; i++) {
+    //  WriteChar((i%5), i);
+    //  Delay(30000);
+    //  };
   }
   
-  return 0;
+  //return 0; never reached
 }
